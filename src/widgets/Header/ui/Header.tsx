5e907505def0186d74304035419894768bb5ec87
Tenadopt <yury.ignatiev@gmail.com>
2024-07-09 11:26:04 +0200

    fix: replace ButtonLink for useNavigate

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,32 +1,29 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
-import { Link as RouterLink, LinkProps as RouterLinkProps } from 'react-router-dom';
+import { AppBar, Toolbar, Button, Box } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
-interface ButtonLinkProps extends Omit<RouterLinkProps, 'to'> {
-    to: string;
-}
+const Header = () => {
+    const navigate = useNavigate();
 
-const ButtonLink = React.forwardRef<HTMLAnchorElement, ButtonLinkProps>((props, ref) => {
-    const { to, ...other } = props;
-    return <RouterLink ref={ref} to={to} {...other} />;
-});
-
-ButtonLink.displayName = 'ButtonLink'; // Указываем displayName для отладки
-
-const Header = () => (
-    <AppBar position="static">
-        <Toolbar>
-            <Typography variant="h6" style={{ flexGrow: 1 }}>
-                Mini Paint
-            </Typography>
-            <Button color="inherit" component={ButtonLink} to="/signin">
-                Sign In
-            </Button>
-            <Button color="inherit" component={ButtonLink} to="/signup">
-                Sign Up
-            </Button>
-        </Toolbar>
-    </AppBar>
-);
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <Box display="flex" alignItems="center" flexGrow={1}>
+                    <Button color="inherit" onClick={() => navigate('/editor')}>
+                        Mini Paint Editor
+                    </Button>
+                </Box>
+                <Box display="flex">
+                    <Button color="inherit" onClick={() => navigate('/signin')}>
+                        Sign In
+                    </Button>
+                    <Button color="inherit" onClick={() => navigate('/signup')}>
+                        Sign Up
+                    </Button>
+                </Box>
+            </Toolbar>
+        </AppBar>
+    );
+};
 
 export default Header;
