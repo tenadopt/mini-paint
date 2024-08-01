cbd5e6dd6f3e9006593f980381ab919c86aa864e
Tenadopt <yury.ignatiev@gmail.com>
2024-08-01 18:33:33 +0200

    fix: env variables

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -3,14 +3,14 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAAJvrhLleIYSXsiVDysFPD0ygbfAN5UmI",
-    authDomain: "mini-paint-65d68.firebaseapp.com",
-    databaseURL: "https://mini-paint-65d68-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "mini-paint-65d68",
-    storageBucket: "mini-paint-65d68.appspot.com",
-    messagingSenderId: "783787819883",
-    appId: "1:783787819883:web:efc9d8ea8db0ea95f9822e",
-    measurementId: "G-15H4T82GHF"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
